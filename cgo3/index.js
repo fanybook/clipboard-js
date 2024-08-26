33a8f74babe2a84fe752559ac01e0f0e90198b38
fanybook <fanybook@gmail.com>
2024-08-26 14:01:42 +0800

    丰富测试脚本

--- cgo3/index.js
+++ cgo3/index.js
@@ -16,33 +16,74 @@ const clipboard = require('./index2.js')
 // clipboard.writeRAWD(Buffer.from('hello rawd666!'))
 // console.log('CT_RAWD:', clipboard.readRAWD().toString())
 //
-// clipboard.writeMany({
-//   text: '你好1',
-//   html: '<p>世界1</p>',
-//   rawd: Buffer.from('hello rawd666!'),
-// })
-//
+clipboard.writeMany({
+  text: '你好1',
+  html: '<p>世界1</p>',
+  rawd: Buffer.from('hello rawd666!'),
+})
+
+console.log('CT_TEXT:', clipboard.readText())
+console.log('CT_HTML:', clipboard.readHTML())
+console.log('CT_RAWD:', clipboard.readRAWD().toString())
+
+
+
+// const fs = require('node:fs')
+// const path = require('node:path')
+// const imagePath = path.join(__dirname, 'example_output.png')
+// // // 读取图片文件并转换为 Buffer
+// // fs.readFile(imagePath, (err, data) => {
+// //     if (err) {
+// //         console.error('Error reading file:', err)
+// //         return
+// //     }q
+// //     clipboard.writeImage(data)
+// // })
+// const data = fs.readFileSync(imagePath)
+// clipboard.writeImage(data)
+
+// // const start0 = async () => {
+// //     for (let idx = 0;; idx++) {
+// //         await (() => {
+// //             return new Promise((resolve, rejuct) => {
+// //                 setTimeout(() => {
+// //                     resolve()
+// //                 }, 1000)
+// //             })
+// //         })()
+// //         console.log("haha:", idx)
+
+// //         if (idx >= 5) {
+// //             // TODO: 有问题，保存的图片打不开（后来试又好了）
+// //             const imageBuffer = clipboard.readImage()
+// //             fs.writeFileSync('./example_output5.png', imageBuffer)
+// //             break
+// //         }
+// //     }
+// // }
+// // start0()
+
+// // TODO: 有问题，保存的图片打不开（后来试又好了）
+// const imageBuffer = clipboard.readImage()
+// fs.writeFileSync('./example_output2.png', imageBuffer)
+
 // console.log('CT_TEXT:', clipboard.readText())
 // console.log('CT_HTML:', clipboard.readHTML())
 // console.log('CT_RAWD:', clipboard.readRAWD().toString())
 
-
-const fs = require('node:fs')
-const path = require('node:path')
-const imagePath = path.join(__dirname, 'example_output.png')
-// // 读取图片文件并转换为 Buffer
-// fs.readFile(imagePath, (err, data) => {
-//     if (err) {
-//         console.error('Error reading file:', err)
-//         return
-//     }q
-//     clipboard.writeImage(data)
-// })
-const data = fs.readFileSync(imagePath)
-clipboard.writeImage(data)
-
-// TODO: 有问题，保存的图片打不开（后来试又好了）
-const imageBuffer = clipboard.readImage()
-fs.writeFileSync('./example_output2.png', imageBuffer)
-
 console.log(666.111, clipboard.getSequenceNumber())
+
+const start = async () => {
+    for (let idx = 0;; idx++) {
+        await (() => {
+            return new Promise((resolve, rejuct) => {
+                setTimeout(() => {
+                    resolve()
+                }, 1000)
+            })
+        })()
+        console.log(idx)
+        console.log(666.111, clipboard.getSequenceNumber())
+    }
+}
+start()
